09162440fd0a25f41199cae6251e16ba51e70454
Ssim02 <simbojm144144144@gmail.com>
2025-06-03 17:21:28 +0900

    [Algorithm][Baekjoon] 1978 소수 찾기 문제 풀이 진행 중

---
+++ Algorithm/Baekjoon/1978/solution.py
@@ -0,0 +1,15 @@
+# 백준 1978번 - 소수 찾기
+
+# 수의 개수 N 입력
+N = int(input())
+
+# N개의 수 입력
+# 슬라이싱으로 N개의 수만 입력되도록 강제제한
+numbers = list(map(int, input().split())) [:N]
+
+# 입력값에서 소수가 몇 개인지 구하기
+count = 0 # 소수의 개수를 체크할 변수
+
+for i in numbers :
+    if i
+
